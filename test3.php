8977fcee0818f57c5d99449dee8e166b850ae1fa
Anonymous121 <anonymous12@riseup.net>
2014-01-25 22:25:15 +0200

    Delete test3.php

--- test3.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-echo "SERVER_ADDR : " . $_SERVER['SERVER_ADDR'] . "<br />";
-echo "REMOTE_ADDR : " . $_SERVER['REMOTE_ADDR'] . "<br />";
-echo "REMOTE_HOST : " . $_SERVER['REMOTE_HOST'] . "<br />";
-?>
