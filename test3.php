c07d674e5ece12561808e344f9b2895b3debf903
Anonymous12 <anonymous12@riseup.net>
2014-01-25 21:15:32 +0100

    Adding new branch: Develoment

---
+++ test3.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "SERVER_ADDR : " . $_SERVER['SERVER_ADDR'] . "<br />";
+echo "REMOTE_ADDR : " . $_SERVER['REMOTE_ADDR'] . "<br />";
+echo "REMOTE_HOST : " . $_SERVER['REMOTE_HOST'] . "<br />";
+?>
